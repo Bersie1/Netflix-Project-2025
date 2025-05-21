f0cc7c84a93c4afadd138b7dc75beaadd8e89e75
Bersie1 <bersabe.tek@gmail.com>
2025-05-21 00:02:26 -0400

    update

--- Netflix-Project/src/Utils/requests.jsx
+++ Netflix-Project/src/Utils/requests.jsx
@@ -1,4 +1,4 @@
-const API_KEY = "ec989f0bedde5daaaaed42db1c333acb";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US=`,
